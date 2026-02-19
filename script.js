451f3f28e830e580d9ee42ffadf1f6a5dcd9eed0
drex109 <96746274+drex109@users.noreply.github.com>
2026-02-19 12:34:27 -0700

    scaffolding game logic

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log('script file linked')
+function Gameboard(symbol) {
+    const gameboard = [
+    [' ', ' ', ' ',],
+    [' ', ' ', ' '],
+    [' ', ' ',' ']
+    ];
+    console.log(gameboard.join('\n'))
+}
+Gameboard();
+
+function Player(name, symbol) {
+    this.name = name;
+    this.symbol = symbol;
+}
+
+function playerTurn(board, playerOne, playerTwo) {
+    playerOne = new Player('player 1', 'X');
+    playerTwo = new Player('player 2', 'O');
+    console.log(playerOne);
+    console.log(playerTwo);
+}
+
+function playGame(board, playerOne, playerTwo) {
+    
+}
+playGame();
